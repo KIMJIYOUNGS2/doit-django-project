43e835ec8a8a8b811dd7699bc2de4cea87df2377
jiyoungkimm <rla1014ss@gmail.com>
2023-01-29 22:31:07 +0900

    CBV로 블로그 포스트 상세 페이지 만들기

--- blog/urls.py
+++ blog/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.PostList.as_view()),
-    path('<int:pk>/', views.single_post_page),
+    path('<int:pk>/', views.PostDetail.as_view()),
 
 ]
