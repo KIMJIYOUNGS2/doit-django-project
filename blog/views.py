d9bf124ec141cbb185b796f0c6b2c596a43c8159
jiyoungkimm <rla1014ss@gmail.com>
2023-01-29 20:01:03 +0900

    FBV로 블로그 포스트 목록 페이지 만들기

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from .models import Post
 
-# Create your views here.
+
+def index(request):
+    posts = Post.objects.all().order_by('-pk')  # 최신글이 제일 위로 나오게 하기
+
+    return render(
+        request,
+        'blog/index.html',
+        {
+            'posts': posts,
+        }
+    )
+
+# render 함수를 통해 템플릿 폴더에서 blog 폴더의 index.html 파일을 찾아서 방문자에게 보내줌
