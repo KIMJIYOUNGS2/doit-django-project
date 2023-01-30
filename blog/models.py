80585a462480698a0e222fa050268ba1e39708df
jiyoungkimm <rla1014ss@gmail.com>
2023-01-30 16:04:52 +0900

    media

--- blog/models.py
+++ blog/models.py
@@ -4,9 +4,11 @@ from django.db import models
 class Post(models.Model):
     title = models.CharField(max_length=30)
     content = models.TextField()
+
+    head_image = models.ImageField(
+        upload_to='blog/images/%Y/%m/%d/', blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-
     # author: 추후에 작성
 
     def __str__(self):
--- doit_django_prj/urls.py
+++ doit_django_prj/urls.py
@@ -1,5 +1,8 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+
 
 urlpatterns = [
     path('blog/', include('blog.urls')),
@@ -7,3 +10,5 @@ urlpatterns = [
     path('', include('single_pages.urls')),
 
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
